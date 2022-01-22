98f99a929858f43632366cc02bf844a1c6cbbb37
v1adislov3 <vladislav.pshebitsin@icloud.com>
2022-01-22 21:14:52 +0200

    hw2

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,8 @@
 let sum = +prompt("Ваш годовой доход");
 
-let arrSingle = [0, 9950, 40525, 86376, 164925, 209425, 523600, Infinity,];
+let arrSingle = [0, 9950, 40525, 86376, 164925, 209425, 523600, Infinity];
 
-let arrRate = [0.10, 0.12, 0.22, 0.24, 0.32, 0.35, 0.37];
+let arrRate = [ 0.10, 0.12, 0.22, 0.24, 0.32, 0.35, 0.37];
 
 const deductSingle = 12550;
 
@@ -10,23 +10,29 @@ let federalTaxe = 0;
 
 let sumDeduct = sum - deductSingle;
 
+console.log(sumDeduct);
 
 if (sumDeduct <= 0) {
     federalTaxe = 0;
 }
 else {
     for (let i = 0; i < arrSingle.length; i++) {
-        let singleLimit = arrSingle[i];
-        let endTaxe = arrSingle[i + 1];
-        let percent = arrRate[i];
-
+        let singleLimit = arrSingle [i];
+        console.log(`singleLimit ${singleLimit}`);
+        let endTaxe = arrSingle [i + 1];
+        console.log(`endTaxe${endTaxe}`);
+        let percent = arrRate [i];
 
     if (sumDeduct > endTaxe) {
             federalTaxe = federalTaxe + ((endTaxe - singleLimit) * percent);
+            console.log(`ONE ${federalTaxe}`);
     } else {
             federalTaxe =federalTaxe + ((sumDeduct - singleLimit) * percent);
+            console.log(`TWO ${federalTaxe}`);
+            break;
         }
-        console.log(federalTaxe);
-    }
+        
+    } 
 }
 
+alert(`Ваш налог ${Math.round(federalTaxe)*100/100}`);
